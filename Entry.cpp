9d7c3f990558de29b33e4d17002fe074021cae84
yangff <yangff1@gmail.com>
2013-02-28 18:05:26 +0800

    Start Project
    TAT

---
+++ Entry.cpp
@@ -0,0 +1,10 @@
+#include <Common.h>
+IGraphics* CreateGraphics(IHost* _host){
+	return NULL;
+};
+IResource* CreateResource(IHost* _host){
+	return NULL;
+};
+ISound* CreateSound(IHost* _host){
+	return NULL;
+};
